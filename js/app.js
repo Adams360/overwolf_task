e92c06fb6fe8d31bbc2ec792064a7759d5577ab6
= <=>
2020-02-20 16:29:14 +0200

    improved project

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let root = document.documentElement;
+let pillar = document.querySelector('.pillar');
+
+
+// root.addEventListener("mousemove", e => {
+//     root.style.setProperty('--pillarHeight', e.clientX + "%");
+//   });
